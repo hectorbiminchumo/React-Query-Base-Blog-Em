08b70fc5482f356d0e01b8b7f17bd46c998f0627
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2022-04-18 07:33:11 -0500

    update refetch options

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -13,7 +13,13 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 minutes
+    cacheTime: 900000, // 15 minutes by default is 5 minutes(doesnt make sense for StaleTime to exceed cachetime)
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
   return data;
 }
 
