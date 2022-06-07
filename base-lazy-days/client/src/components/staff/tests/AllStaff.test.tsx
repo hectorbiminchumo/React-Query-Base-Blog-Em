e843b299eb7c8bc52f7a34df532856a478a22424
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2022-06-07 09:38:58 -0500

    code quiz

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,8 +1,10 @@
-import { screen } from '@testing-library/react';
+/* eslint-disable no-console */
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { QueryClientProvider, setLogger } from 'react-query';
 
-// import { rest } from 'msw';
-// import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
+import { server } from '../../../mocks/server';
+import { generateQueryClient } from '../../../react-query/queryClient';
 import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
@@ -17,9 +19,35 @@ test('renders response from query', async () => {
 
 test('handles query error', async () => {
   // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.resetHandlers(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
 });
+
+// suppress errors
+setLogger({
+  log: console.log,
+  warn: console.warn,
+  error: () => {
+    // swallow errors without printing out
+  },
+});
+
+// set up query client with retries set to false
+const queryClient = generateQueryClient();
+const options = queryClient.getDefaultOptions();
+options.queries = { ...options.queries, retry: false };
+queryClient.setDefaultOptions(options);
+
+// render wrapped with provider
+render(
+  <QueryClientProvider client={queryClient}>
+    <AllStaff />
+  </QueryClientProvider>,
+);
+
+// check for the toast alert
+const alertToast = await screen.findByRole('alert');
+expect(alertToast).toHaveTextContent('Request failed with status code 500');
